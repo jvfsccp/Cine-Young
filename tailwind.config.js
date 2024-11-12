f592d49e205cbfd8bac261f0bdcc892e0637835a
João Victor <150389471+jvfsccp@users.noreply.github.com>
2024-11-12 15:34:11 -0300

    Pagina de registro

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'bg-register': '#3A4144',
+      'white': '#FFFFFF',
+      'input-color': '#B0C0D4',
+      'btn-register': '#4E11DB'
+      
+    },
+    fontFamily:{
+      'sans': ['Inter', 'sans-serif']
+   },
+    extend: {},
+  },
+  plugins: [],
+}
+
